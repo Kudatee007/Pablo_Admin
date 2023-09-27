c5f7e9e254cca5efe7fdd319de1c67deb82d4b02
kudaT <timilehinkudaisi01@gmail.com>
2023-09-27 20:20:17 +0100

    update changes for layout-sider

--- src/components/MainLayout.js
+++ src/components/MainLayout.js
@@ -1,75 +1,149 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import {
-    MenuFoldOutlined,
-    MenuUnfoldOutlined,
-    UploadOutlined,
-    UserOutlined,
-    VideoCameraOutlined,
-  } from '@ant-design/icons';
-  import { Layout, Menu, Button, theme } from 'antd';
-  const { Header, Sider, Content } = Layout;
-  const MainLayout = () => {
-    const [collapsed, setCollapsed] = useState(false);
-    const {
-      token: { colorBgContainer },
-    } = theme.useToken();
-    return (
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+  UploadOutlined,
+  VideoCameraOutlined,
+} from "@ant-design/icons";
+import {
+  AiOutlineDashboard,
+  AiOutlineShoppingCart,
+  AiOutlineUserAdd,
+  AiOutlineBgColors,
+} from "react-icons/ai";
+import { SiBrandfolder } from "react-icons/si";
+import { BiCategory } from "react-icons/bi";
+import { FaClipboardList } from "react-icons/fa";
+import { Layout, Menu, Button, theme } from "antd";
+import { useNavigate, Outlet } from "react-router-dom";
+const { Header, Sider, Content } = Layout;
+
+const MainLayout = () => {
+  const [collapsed, setCollapsed] = useState(false);
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
+  const navigate = useNavigate();
+  return (
+    <Layout>
+      <Sider trigger={null} collapsible collapsed={collapsed}>
+        <div className="logo">
+          <h2 className="text-white fs-5 text-center py-1 mb-0">
+            <span className="sm-logo">P_D</span>
+            <span className="lg-logo">Pablo_Dimension</span>
+          </h2>
+        </div>
+        <Menu
+          theme="dark"
+          mode="inline"
+          defaultSelectedKeys={[""]}
+          onClick={({ key }) => {
+            if (key === "signout") {
+            } else {
+              navigate(key);
+            }
+          }}
+          items={[
+            {
+              key: "",
+              icon: <AiOutlineDashboard className="fs-4" />,
+              label: "Dashboard",
+            },
+            {
+              key: "customers",
+              icon: <AiOutlineUserAdd className="fs-4" />,
+              label: "Customers",
+            },
+            {
+              key: "catalog",
+              icon: <AiOutlineShoppingCart className="fs-4" />,
+              label: "Catalog",
+              children: [
+                {
+                  key: "product",
+                  icon: <AiOutlineDashboard className="fs-5" />,
+                  label: "Add Product",
+                },
+                {
+                  key: "product-list",
+                  icon: <AiOutlineDashboard className="fs-5" />,
+                  label: "Product List",
+                },
+                {
+                  key: "brand",
+                  icon: <SiBrandfolder className="fs-5" />,
+                  label: "Brand",
+                },
+                {
+                  key: "brand-list",
+                  icon: <SiBrandfolder className="fs-5" />,
+                  label: "Brand List",
+                },
+                {
+                  key: "category",
+                  icon: <BiCategory className="fs-5" />,
+                  label: "Category",
+                },
+                {
+                  key: "category-list",
+                  icon: <BiCategory className="fs-5" />,
+                  label: "Category List",
+                },
+                {
+                  key: "color",
+                  icon: <AiOutlineBgColors className="fs-5" />,
+                  label: "Color",
+                },
+                {
+                  key: "color-list",
+                  icon: <AiOutlineBgColors className="fs-5" />,
+                  label: "Color List",
+                },
+              ],
+            },
+            {
+              key: "orders",
+              icon: <FaClipboardList className="fs-5" />,
+              label: "Orders",
+            },
+            {
+              key: "enquiries",
+              icon: <FaClipboardList className="fs-5" />,
+              label: "Enquiries",
+            },
+          ]}
+        />
+      </Sider>
       <Layout>
-        <Sider trigger={null} collapsible collapsed={collapsed}>
-          <div className="logo" />
-          <Menu
-            theme="dark"
-            mode="inline"
-            defaultSelectedKeys={['1']} 
-            items={[
-              {
-                key: '1',
-                icon: <UserOutlined />,
-                label: 'nav 1',
-              },
-              {
-                key: '2',
-                icon: <VideoCameraOutlined />,
-                label: 'nav 2',
-              },
-              {
-                key: '3',
-                icon: <UploadOutlined />,
-                label: 'nav 3',
-              },
-            ]}
+        <Header
+          style={{
+            padding: 0,
+            background: colorBgContainer,
+          }}
+        >
+          <Button
+            type="text"
+            icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+            onClick={() => setCollapsed(!collapsed)}
+            style={{
+              fontSize: "16px",
+              width: 64,
+              height: 64,
+            }}
           />
-        </Sider>
-        <Layout>
-          <Header
-            style={{
-              padding: 0,
-              background: colorBgContainer,
-            }}
-          >
-            <Button
-              type="text"
-              icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
-              onClick={() => setCollapsed(!collapsed)}
-              style={{
-                fontSize: '16px',
-                width: 64,
-                height: 64,
-              }}
-            />
-          </Header>
-          <Content
-            style={{
-              margin: '24px 16px',
-              padding: 24,
-              minHeight: 280,
-              background: colorBgContainer,
-            }}
-          >
-            Content
-          </Content>
-        </Layout>
+        </Header>
+        <Content
+          style={{
+            margin: "24px 16px",
+            padding: 24,
+            minHeight: 280,
+            background: colorBgContainer,
+          }}
+        >
+         <Outlet />
+        </Content>
       </Layout>
-    );
-  };
-  export default MainLayout;
+    </Layout>
+  );
+};
+export default MainLayout;
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Dashboard = () => {
   return (
-    <div>Dasgboard</div>
+    <div>Dashboard</div>
   )
 }
 
